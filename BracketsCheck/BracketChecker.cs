aa9d09ff791f97c87ff343340c68b1061021c320
Xopabyteh <xopabyteh1337@gmail.com>
2022-03-24 16:49:42 +0100

    Add project files.

---
+++ BracketsCheck/BracketChecker.cs
@@ -0,0 +1,55 @@
+﻿using System.Linq;
+
+namespace BracketsCheck;
+
+public class BracketChecker
+{
+    private static readonly (char, int)[] OpeningBracketsTuple = new (char, int)[]
+    {
+        ('(',1),
+        ('[',2),
+        ('{',3)
+    };
+    private static readonly (char, int)[] ClosingBracketsTuple = new (char, int)[]
+    {
+        (')',1),
+        (']',2),
+        ('}',3)
+    };
+    private bool Test(string inp)
+    {
+        Stack<int> bracketIDStack = new Stack<int>();
+        foreach (var c in inp)  
+        {
+
+            //Case opening bracket
+            foreach ((char, int) valueTuple in OpeningBracketsTuple)
+            {
+                if (valueTuple.Item1.Equals(c))
+                {
+                    bracketIDStack.Push(valueTuple.Item2);
+                }
+            }
+            //Case closing bracket
+            foreach (var valueTuple in ClosingBracketsTuple)
+            {
+                if (valueTuple.Item1.Equals(c))
+                {
+                    int lastBrackedID = bracketIDStack.Pop();
+                    if (lastBrackedID != valueTuple.Item2)
+                    {
+                        return false;
+                    }
+                }
+            }
+        }
+
+        return true;
+    }
+
+    public void WriteTest(string inp, bool expected)
+    {
+        bool result = Test(inp);
+        Console.WriteLine($"{(expected == result ? "SUCCESS" : "FAIL")} \t inp: {inp}, expect: {expected}, result: {result}");
+    }
+}
---
+++ BracketsCheck/Program.cs
@@ -0,0 +1,20 @@
+﻿// ( { [ ] } )
+// ( -> push
+// ) -> pop
+
+using BracketsCheck;
+
+BracketChecker bc = new BracketChecker();
+bc.WriteTest("{[()]}", true);
+bc.WriteTest("{(})", false);
+bc.WriteTest("{[(])}", false);
+bc.WriteTest("{{[[(())]]}}", true);
+bc.WriteTest("(){}[]", true);
+bc.WriteTest("(]", false);
+bc.WriteTest("[(])", false);
+bc.WriteTest("[({})](]", false);
+bc.WriteTest("", true);
+bc.WriteTest("pepa", true);
+bc.WriteTest("(pepa)", true);
+bc.WriteTest("10 * [123 + 7 (6 + 2)]", true);
+bc.WriteTest("10 * [123 + 7 (6 + 2))", false);
